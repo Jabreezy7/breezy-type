c36cfcc89d05f4cb301627c455b804f61792d30a
Jabreezy7 <mohammedbaled7@gmail.com>
2024-10-12 18:31:15 -0400

    Add Cursor Blinking Animation

--- typing.js
+++ typing.js
@@ -5,28 +5,42 @@ const words = [
     "Egg", "Fish", "Grass", "Hat", "Island", "Jacket", "King", "Light", "Mountain", "Nest",
     "Orange", "Pencil", "Quilt", "Rain", "Snow", "Train", "Unicorn", "Volcano", "Window",
     "Yacht", "Zipper"
-  ];
+];
 
-function randomWord(){
+function addClass(el, name) {
+    el.className += ' ' + name;
+}
+
+function removeClass(el, name) {
+    el.className = el.className.replace(name, "");
+}
+
+function randomWord() {
     const randomIndex = Math.floor(Math.random() * words.length);
     return words[randomIndex].toLowerCase();
 }
 
-function formatWord(word){
-    return `<div class = "word">
-    <span class="letter">
-    ${word.split('').join('</span><span class="letter">')}
-    </span>
-    </div>`;
+function formatWord(word) {
+    return `<div class = "word"><span class="letter">${word.split('').join('</span><span class="letter">')}</span></div>`;
 }
 
-  
-function newGame(){
+
+function newGame() {
     document.getElementById("words").innerHTML = "";
-    for (let i = 0; i < 200; i++){
+    for (let i = 0; i < 200; i++) {
         document.getElementById("words").innerHTML += formatWord(randomWord());
     }
+    addClass(document.querySelector(".word"), "current");
+    addClass(document.querySelector(".letter"), "current");
 }
 
+
+document.getElementById("game").addEventListener("keyup", ev => {
+    const key = ev.key;
+    const currentLetter = document.querySelector(".letter.current");
+    const expected = currentLetter.innerHTML;
+
+    console.log({key, expected});
+})
 
 newGame();
